25bca9cd2048d65987bb07432e4659bf2025e953
flaviostutz <flaviostutz@gmail.com>
2020-07-08 01:52:15 -0300

    initial version

---
+++ startup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+# set -x
+
+if [ "$CONFIG_SERVER_NODES" == "" ]; then
+    echo "CONFIG_SERVER_NODES is required"
+    exit 1
+fi
+
+/config.sh &
+
+echo "Starting Mongo configsrv..."
+mongod --port 27017 --configsvr --replSet $CONFIG_REPLICA_SET_NAME --bind_ip_all
