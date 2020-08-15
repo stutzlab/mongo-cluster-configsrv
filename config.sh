4ed42d6beb5b7afdfe3034d659692cdf11ca09ed
flaviostutz <flaviostutz@gmail.com>
2020-08-15 17:35:12 -0300

    simplyfing health check

--- config.sh
+++ config.sh
@@ -52,12 +52,15 @@ IFS=',' read -r -a NODES <<< "$INIT_CONFIG_NODES"
 S=""
 c=0
 for N in "${NODES[@]}"; do
-   echo " - $N"
+   echo ">>> Node $N"
    echo "${S}{ _id: $c, host : \"$N:27017\" }" >> /init-configserver.js
    S=","
    c=$((c+1))
    echo " - Waiting for host $N to be available..."
    until ping -c1 $N >/dev/null; do sleep 2; done
+   echo " - Host available. Waiting port 27017..."
+   while ! nc -z $N 27017; do sleep 1; done
+   echo " - Host $N OK"
 done
 
 cat <<EOT >> /init-configserver.js
